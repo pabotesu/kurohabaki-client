1609169e35dca25083cc939208928d51ac8d1395
pabotesu <pab0te2u@gmail.com>
2025-06-10 23:25:20 +0900

    loggerを調整

--- main.go
+++ main.go
@@ -2,37 +2,11 @@ package main
 
 import (
 	"log"
-	"os"
 
 	"github.com/pabotesu/kurohabaki-client/cmd"
-	"golang.zx2c4.com/wireguard/device"
 )
 
-// カスタムWriterを定義（log.Writer に渡せるように）
-type logWriter struct {
-	write func(format string, args ...interface{})
-}
-
-func (lw logWriter) Write(p []byte) (n int, err error) {
-	lw.write("%s", string(p))
-	return len(p), nil
-}
-
 func main() {
-	// ログファイルを開く
-	logFile, err := os.OpenFile("/tmp/client.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-	if err != nil {
-		log.Fatalf("❌ Failed to open log file: %v", err)
-	}
-	defer logFile.Close()
-	// WireGuard-Go の Logger を使って統合
-	wgLogger := device.NewLogger(device.LogLevelVerbose, "kurohabaki")
-
-	// log パッケージに WireGuard の Logger を流用
-	log.SetOutput(logWriter{write: wgLogger.Verbosef})
-	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
-
 	log.Println("kurohabaki client starting...")
-	log.Println("init log: should go through custom logger")
 	cmd.Execute()
 }
