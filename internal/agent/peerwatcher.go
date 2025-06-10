4c18e55b31f94fceda394a931c398bfafe2f1507
pabotesu <pab0te2u@gmail.com>
2025-06-10 13:30:35 +0900

    peer追加機能のデバッグ

--- internal/agent/peerwatcher.go
+++ internal/agent/peerwatcher.go
@@ -31,6 +31,11 @@ func StartPeerWatcher(ctx context.Context, cli *clientv3.Client, wgIf *wg.WireGu
 				continue
 			}
 
+			log.Println("[PeerWatcher] Fetched nodes from etcd:")
+			for _, node := range nodes {
+				log.Printf("[PeerWatcher] etcd node: PubKey=%s, IP=%s, Endpoint=%s", node.PublicKey, node.IP, node.Endpoint)
+			}
+
 			currentPeers, err := wg.ConvertNodesToPeers(nodes)
 			if err != nil {
 				log.Printf("Failed to convert nodes to peers: %v", err)
@@ -47,4 +52,5 @@ func StartPeerWatcher(ctx context.Context, cli *clientv3.Client, wgIf *wg.WireGu
 			}
 		}
 	}
+
 }
