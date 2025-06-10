78b367ba9c31431976d48f8dca49e93c37c6ce19
pabotesu <pab0te2u@gmail.com>
2025-06-10 14:06:43 +0900

    peer追加機能のデバッグ

--- cmd/up.go
+++ cmd/up.go
@@ -4,9 +4,7 @@ import (
 	"context"
 	"encoding/base64"
 	"fmt"
-	"io"
 	"log"
-	"os"
 	"time"
 
 	"github.com/pabotesu/kurohabaki-client/config"
@@ -77,15 +75,6 @@ var upCmd = &cobra.Command{
 }
 
 func init() {
-
-	logFile, err := os.OpenFile("/tmp/client.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-	if err != nil {
-		log.Fatalf("❌ Failed to open log file: %v", err)
-	}
-	multi := io.MultiWriter(os.Stdout, logFile)
-	log.SetOutput(multi)
-	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
-
 	rootCmd.AddCommand(upCmd)
 	upCmd.Flags().StringVar(&configPath, "config", "config.yaml", "Path to config file")
 }
--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"io"
 	"log"
+	"os"
 
 	"github.com/pabotesu/kurohabaki-client/cmd"
 )
 
 func main() {
+	logFile, err := os.OpenFile("/tmp/client.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatalf("❌ Failed to open log file: %v", err)
+	}
+	multi := io.MultiWriter(os.Stdout, logFile)
+	log.SetOutput(multi)
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+
 	log.Println("kurohabaki client starting...")
 	cmd.Execute()
 }
